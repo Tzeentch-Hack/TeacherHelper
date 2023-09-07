74aa60e6ce3e6f2d9d89a34500e9d588f598ca78
Blazer20 <48653552+Blazer20@users.noreply.github.com>
2023-09-07 15:17:09 +0500

    Add DetailsScreen

--- TeacherHelperAndroid/androidApp/src/main/java/com/tzeentch/teacherhelper/android/AppState.kt
+++ TeacherHelperAndroid/androidApp/src/main/java/com/tzeentch/teacherhelper/android/AppState.kt
@@ -2,10 +2,10 @@ package com.tzeentch.teacherhelper.android
 
 object AuthenticationSection {
     const val LOGIN_ROUTE = "loginRoute"
-    const val REGISTRATION_ROUTE = "registrationRoute"
 }
 
 object MainSection {
     const val OPTION_ROUTE = "optionRoute"
     const val CAMERA_ROUTE = "cameraRoute"
+    const val DETAILS_ROUTE = "detailsRoute"
 }
