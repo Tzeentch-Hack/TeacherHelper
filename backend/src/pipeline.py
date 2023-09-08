3983ada982525d826dd7f682e5fdb74605445fbf
DostavalovID <igor.d@ptolemay.com>
2023-09-08 10:32:35 +0500

    Final push backend

--- backend/src/pipeline.py
+++ backend/src/pipeline.py
@@ -2,6 +2,8 @@ from backend.src.models import picture_generator, text_generator, text_recogniti
 from backend.src.models.text_recognition import recognize_image
 from backend.src import presentation_builder, text_analyzer
 import cv2
+import os
+
 
 class Pipeline:
     def __init__(self):
@@ -12,6 +14,7 @@ class Pipeline:
         self.text_analyzer = text_analyzer.TextAnalyzer(self.text_analyzer_model, self.text_analyzer_max_tokens)
 
     def recognize_images(self, image_paths):
+        print('image_paths:', image_paths)
         image_files = []
         for image_path in image_paths:
             image_files.append(cv2.imread(image_path))
@@ -20,10 +23,11 @@ class Pipeline:
             total_text += recognize_image(image_file) + '\n'
         return total_text
 
-
     def infer_first_step(self, text):
-        analyzed_presentation_source, analyzed_presentation_eng, source_text = self.text_analyzer.analyze_presentation(text)
+        analyzed_presentation_source, analyzed_presentation_eng, source_text = self.text_analyzer.analyze_presentation(
+            text)
         picture_descriptions = self.text_analyzer.get_picture_descriptions(analyzed_presentation_eng)
+        print('descriptions:', picture_descriptions)
         return analyzed_presentation_source, picture_descriptions, source_text
 
     def get_one_slide(self, title, phrases, img_path, output_path):
@@ -36,17 +40,20 @@ class Pipeline:
         analyzed_json = self.text_analyzer.analyze_text(text)
         return analyzed_json
 
-    def infer_whole(self, input_paths):
+    def infer_whole(self, input_paths, output_path):
         text = self.recognize_images(input_paths)
         analyzed_presentation_source, picture_descriptions, source_text = self.infer_first_step(text)
         img_paths = []
         for i, picture_description in enumerate(picture_descriptions, 1):
-            img_path = f"../data/user/images/{i}.jpg"
+            img_path = os.path.join(output_path, f"{i}.jpg")
             self.pic_gen.generate_image(picture_description, output_path=img_path)
             img_paths.append(img_path)
+
         for d, s in zip(analyzed_presentation_source, img_paths):
             d["img_path"] = s
-        self.get_presentation(analyzed_presentation_source, output_path="../data/user/presentations/presentation.pptx")
+        for info_for_slide in analyzed_presentation_source:
+            presentation_builder.create_pptx_and_convert_to_jpg(info_for_slide['topic_name'], info_for_slide['options'], info_for_slide['img_path'], info_for_slide['img_path'][:-4])
+        self.get_presentation(analyzed_presentation_source, output_path=os.path.join(output_path, "temp.pptx"))
         analyzed_json = self.infer_second_step(text)
         return (source_text, analyzed_json['teaching_recommendations'], analyzed_json['lesson_structure_and_estimates'],
                 analyzed_json['possible_questions'],)
